de5a1849ca91c630939249d2f60e5f3bcf63f876
Sreeraksha M R <raksharamesh1998@gmail.com>
2023-05-23 17:41:16 +0530

    adding grid feature

--- js_tasks/task10_gridVsflex/script.js
+++ js_tasks/task10_gridVsflex/script.js
@@ -1,27 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Get the modal
-    var modal = document.getElementById("myModal");
-  
+    var modal1 = document.getElementById("myModal1");
+    var modal2 = document.getElementById("myModal2");
     // Get the button that opens the modal
-    var btn = document.getElementById("btn");
-  
+    var btn1 = document.getElementById("btn1");
+    var btn2 = document.getElementById("btn2");
     // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
   
     // When the user clicks the button, open the modal 
-    btn.addEventListener("click", function() {
-      modal.style.display = "block";
+    btn1.addEventListener("click", function() {
+      modal1.style.display = "block";
     });
+
+    btn2.addEventListener("click", function() {
+        modal2.style.display = "block";
+      });
   
     // When the user clicks on <span> (x), close the modal
     span.addEventListener("click", function() {
-      modal.style.display = "none";
+      modal1.style.display = "none";
+      modal2.style.display = "none";
     });
   
     // When the user clicks anywhere outside of the modal, close it
     window.addEventListener("click", function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
+      if (event.target == modal1 || event.target == modal2){
+        modal1.style.display = "none";
+        modal2.style.display = "none";
       }
     });
 });
