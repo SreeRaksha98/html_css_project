421733435f6e014c8f5a2adab74bbff80425a19e
Sreeraksha M R <raksharamesh1998@gmail.com>
2023-05-27 16:16:40 +0530

    adding eventlistner to correct button

--- js_tasks/task15_todo/script.js
+++ js_tasks/task15_todo/script.js
@@ -1,24 +1,38 @@
 var mainContainerVar = document.querySelector(".main-container")
-var subContainervar = document.createElement('div')
-subContainervar.classList.add("sub-container")
-var inputTag = document.createElement('input')
-var btnSubmmit = document.createElement('button')
-btnSubmmit.innerHTML = 'submit'
-btnSubmmit.classList.add('btn-submit')
-var btnClose = document.createElement('button')
-btnClose.classList.add('btn-close')
-btnClose.innerHTML = 'close'
 
-document.addEventListener('click', () => {
+// creating add button
+var btnAdd = document.getElementById('new-todo')
+
+btnAdd.addEventListener('click', () => {
+
+    // creating sub-container
+    var subContainervar = document.createElement('div')
+    subContainervar.classList.add("sub-container")
+
+    // creating input tag 
+    var inputTag = document.createElement('input')
     inputTag.name = 'input'
     inputTag.setAttribute('id', 1)
     inputTag.value = ''
-    btnSubmmit.addEventListener('click', () => {
-        console.log('clicked')
-    })
-    subContainervar.append(inputTag, btnSubmmit, btnClose) 
+
+    // creating submit button
+    var btnSave = document.createElement('button')
+    btnSave.innerHTML = 'save'
+    btnSave.classList.add('btn-submit')
+
+    // creating close button
+    var btnClose = document.createElement('button')
+    btnClose.classList.add('btn-close')
+    btnClose.innerHTML = 'close'
+
+    // btnSubmmit.addEventListener('click', () => {
+    //     console.log('clicked')
+    // })
+    subContainervar.append(inputTag, btnSave, btnClose) 
     mainContainerVar.append(subContainervar)
 })
+
+
 
 
 
