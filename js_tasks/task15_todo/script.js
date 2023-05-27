6b922859b7e08f3a357149bfe48ad49272b3859a
Sreeraksha M R <raksharamesh1998@gmail.com>
2023-05-28 00:02:40 +0530

    adding loacal storage concept

--- js_tasks/task15_todo/script.js
+++ js_tasks/task15_todo/script.js
@@ -3,33 +3,47 @@ var mainContainerVar = document.querySelector(".main-container")
 // creating add button
 var btnAdd = document.getElementById('new-todo')
 
+// Adding new notes 
 btnAdd.addEventListener('click', () => {
+    if(!document.getElementById('todo-add-container')){   
+        // creating sub-container
+        var subContainervar = document.createElement('div')
+        subContainervar.classList.add("sub-container")
+        subContainervar.id = 'todo-add-container'    //important step
 
-    // creating sub-container
-    var subContainervar = document.createElement('div')
-    subContainervar.classList.add("sub-container")
+        // creating input tag 
+        var inputTag = document.createElement('input')
+        inputTag.name = 'input'
+        inputTag.setAttribute('id', 1)
+        inputTag.classList.add("input-tag")
 
-    // creating input tag 
-    var inputTag = document.createElement('input')
-    inputTag.name = 'input'
-    inputTag.setAttribute('id', 1)
-    inputTag.value = ''
+        // creating submit button
+        var btnSave = document.createElement('button')
+        btnSave.innerHTML = 'save'
+        btnSave.classList.add('btn-submit')
 
-    // creating submit button
-    var btnSave = document.createElement('button')
-    btnSave.innerHTML = 'save'
-    btnSave.classList.add('btn-submit')
+        btnSave.addEventListener('click', () => {
+            if(!localStorage.getItem('todo-id')){
+                localStorage.setItem('todo-id', JSON.stringify([inputTag.value]))
+            }
+            else{
+                var todoList = localStorage.getItem('todo-id')
+                // console.log(todoList, JSON.parse(todoList))
+                localStorage.setItem('todo-id', JSON.stringify(todoList.concat(inputTag.value)))   
+            }  
+        })
 
-    // creating close button
-    var btnClose = document.createElement('button')
-    btnClose.classList.add('btn-close')
-    btnClose.innerHTML = 'close'
+        // creating close button
+        var btnClose = document.createElement('button')
+        btnClose.classList.add('btn-close')
+        btnClose.innerHTML = 'close'
+        btnClose.addEventListener('click', () => {
+            document.getElementById('todo-add-container').remove()
+        })
 
-    // btnSubmmit.addEventListener('click', () => {
-    //     console.log('clicked')
-    // })
-    subContainervar.append(inputTag, btnSave, btnClose) 
-    mainContainerVar.append(subContainervar)
+        subContainervar.append(inputTag, btnSave, btnClose) 
+        mainContainerVar.append(subContainervar)
+    }
 })
 
 
